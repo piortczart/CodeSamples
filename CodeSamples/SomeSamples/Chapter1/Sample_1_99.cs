7a3c42d3f41e7f1d7fe29fb51727eaae5d6216b2
ef <ef@GTH-EF>
2016-10-21 14:47:39 +0200

    Finally merged, works.

--- CodeSamples/SomeSamples/Chapter1/Sample_1_99.cs
+++ CodeSamples/SomeSamples/Chapter1/Sample_1_99.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace SomeSamples
+namespace SomeSamples.Chapter1
 {
     public class SpecialThing : ThingBase
     {
